a71a833d6734cc86e098bd1955baa42d73f9122e
Mauricio Basalo <mbasalogutierrez@gmail.com>
2024-11-12 14:29:52 -0300

    about

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -8,20 +8,19 @@ function AboutCard() {
       <Card.Body>
         <blockquote className="blockquote mb-0">
           <p style={{ textAlign: "justify" }}>
-            Hi Everyone, I am <span className="purple">Soumyajit Behera </span>
-            from <span className="purple"> Bhubaneswar, India.</span>
+            Hi, I am <span className="purple">Mauricio Basalo </span>
+            dialing in from the quiet yet vibrant corner of <span className="purple">  Maldonado, Uruguay.</span>
             <br />
-            I am currently employed as a software developer at Juspay.
+            I am currently a <span className="purple"> freelance Web Developer</span> and
             <br />
-            I have completed Integrated MSc (IMSc) in Maths and Computing at BIT
-            Mesra.
+            I'm the kind of person who believes in blending technology and top-notch customer service—and having fun while doing it.
+  
             <br />
             <br />
-            Apart from coding, some other activities that I love to do!
           </p>
-          <ul>
+          {/* <ul>
             <li className="about-activity">
-              <ImPointRight /> Playing Games
+              <ImPointRight /> Playing Music
             </li>
             <li className="about-activity">
               <ImPointRight /> Writing Tech Blogs
@@ -29,12 +28,12 @@ function AboutCard() {
             <li className="about-activity">
               <ImPointRight /> Travelling
             </li>
-          </ul>
-
+          </ul> */}
+{/* 
           <p style={{ color: "rgb(155 126 172)" }}>
             "Strive to build things that make a difference!"{" "}
           </p>
-          <footer className="blockquote-footer">Soumyajit</footer>
+          <footer className="blockquote-footer">Soumyajit</footer> */}
         </blockquote>
       </Card.Body>
     </Card>
--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
-import myImg from "../../Assets/avatar.svg";
+import myImg from "../../Assets/avatar3.svg";
 import Tilt from "react-parallax-tilt";
 import {
   AiFillGithub,
@@ -46,7 +46,7 @@ function Home2() {
           </Col>
           <Col md={4} className="myAvtar">
             <Tilt>
-              <img src={myImg} className="img-fluid" alt="avatar" />
+              <img src={myImg} className="img-fluid avatar" alt="avatar" />
             </Tilt>
           </Col>
         </Row>
